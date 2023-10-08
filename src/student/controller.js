c9665c746660c160c6a47c4e4c0cf459a1c5bbf8
Pablo <pablito@me.com>
2023-10-08 19:31:48 +0100

    Added route and query for adding a new student

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,3 +1,4 @@
+const { raw } = require('express');
 const pool = require('../../db');
 const queries = require('./queries');
 
@@ -12,13 +13,33 @@ const getStudentById = (req, res) => {
   const id = parseInt(req.params.id);
 
   pool.query(queries.getStudentById, [id], (error, results) => {
-    console.log(id)
+    console.log(id);
     if (error) throw error;
     res.status(200).json(results.rows);
+  });
+};
+
+const addStudent = (req, res) => {
+  const { name, email, age, dob } = req.body;
+
+  // check if email exists
+  pool.query(queries.checkEmailExists, [email], (error, results) => {
+    if (results.rows.length) {
+      res.send("That email already exists.");
+    } else {
+      pool.query(queries.addStudent, [name, email, age, dob], (error, results) => {
+        console.log(name, email, age, dob);
+        if (error) throw error;
+        res.status(201).send(`Entry created successfully for ${name}.`);
+      });
+    }
   })
-}
+
+  
+};
 
 module.exports = {
   getStudents,
-  getStudentById
+  getStudentById,
+  addStudent,
 };
--- src/student/queries.js
+++ src/student/queries.js
@@ -1,7 +1,12 @@
-const getStudents = "SELECT * FROM students";
-const getStudentById = "SELECT * FROM students WHERE id = $1";
+const getStudents = 'SELECT * FROM students';
+const getStudentById = 'SELECT * FROM students WHERE id = $1';
+const checkEmailExists = 'SELECT * FROM students WHERE email = $1';
+const addStudent =
+  'INSERT INTO students (name, email, age, dob) VALUES ($1, $2, $3, $4)';
 
 module.exports = {
   getStudents,
   getStudentById,
+  addStudent,
+  checkEmailExists,
 };
